e4ab0e6876bcfe7ce6091dea1ec4e72ce135c44c
zusoomro <zusoomro@seas.upenn.edu>
2020-10-18 21:06:26 -0400

    Moves knexfile

--- api/src/initializeDb.ts
+++ api/src/initializeDb.ts
@@ -1,6 +1,6 @@
 import { Model } from "objection";
 import Knex from "knex";
-import knexConfig from "./knexfile";
+import knexConfig from "../knexfile";
 
 const initializeDb = () => {
   const knex = Knex(knexConfig.development);
