f805f5e6df9936470a3f7043b5c5ff301e1e6509
zusoomro <zusoomro@seas.upenn.edu>
2020-10-20 14:46:08 -0400

    Registration style

--- mobile/src/Register.tsx
+++ mobile/src/Register.tsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { View, Text, TextInput, Button, SafeAreaView } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  SafeAreaView,
+  StyleSheet,
+} from "react-native";
 import { Formik } from "formik";
 import { register } from "./authReducer";
 import { useDispatch } from "react-redux";
@@ -8,7 +15,7 @@ const Register: React.FC<{}> = () => {
   const dispatch = useDispatch();
 
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <Formik
         initialValues={{ email: "", password: "" }}
         onSubmit={(values) => {
@@ -18,17 +25,21 @@ const Register: React.FC<{}> = () => {
       >
         {({ handleChange, handleBlur, handleSubmit, values }) => (
           <View>
+            <Text style={styles.heading}>Register</Text>
             <TextInput
               onChangeText={handleChange("email")}
               onBlur={handleBlur("email")}
               value={values.email}
+              placeholder="email"
+              style={styles.input}
             />
             <TextInput
               onChangeText={handleChange("password")}
               onBlur={handleBlur("password")}
               value={values.password}
+              placeholder="password"
+              style={styles.input}
             />
-
             <Button onPress={handleSubmit} title="Submit" />
           </View>
         )}
@@ -36,5 +47,29 @@ const Register: React.FC<{}> = () => {
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    backgroundColor: "white",
+    marginBottom: 20,
+    paddingHorizontal: 20,
+    width: 300,
+    height: 50,
+    fontSize: 16,
+    color: "black",
+    borderRadius: 20,
+  },
+  heading: {
+    fontSize: 20,
+    textAlign: "center",
+    marginBottom: 30,
+  },
+});
 
 export default Register;
