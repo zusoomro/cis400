20aebec79a79dc9515e09fe6fca606e0484af6b5
zusoomro <zusoomro@seas.upenn.edu>
2020-10-26 11:37:14 -0400

    Adds logout, centralizes styles

--- mobile/src/Settings.tsx
+++ mobile/src/Settings.tsx
@@ -1,12 +1,29 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button, StyleSheet, SafeAreaView } from "react-native";
+import { useSelector, useDispatch } from "react-redux";
+import { logOut } from "./authSlice";
 
 const Settings: React.FC<{}> = () => {
+  const user = useSelector((state) => state.auth.user);
+  const dispatch = useDispatch();
+
   return (
-    <View>
-      <Text>Settings</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Settings Page</Text>
+      <Text>Current user: {JSON.stringify(user)}</Text>
+      <Button title="Log out" onPress={() => dispatch(logOut())}></Button>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 10,
+  },
+});
 
 export default Settings;
