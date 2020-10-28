69af8caec4aa9769b0aa842239a7183d9bf6f41b
allysmith12 <allison.smith@seas.upenn.edu>
2020-10-28 17:47:24 -0400

    Remove unused imports

--- mobile/src/events/ScheduleNavigator.tsx
+++ mobile/src/events/ScheduleNavigator.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Button, SafeAreaView, StyleSheet, View } from "react-native";
+import React from "react";
+import {StyleSheet } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import CreateEvent from "./CreateEvent";
