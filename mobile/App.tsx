39d124e16562cc157d827e9dd0c799670479a7bb
chloeprezelski <57121051+chloeprezelski@users.noreply.github.com>
2020-10-28 20:57:13 -0400

    Create pod (#18)
    * hacked implementation -- will fix
    
    * "idk pulled zulfis code"
    
    * "import upper case"
    
    * Merge branch 'main' of https://github.com/zusoomro/cis400 into create-pod
    
    * stable version -- push to main in order to get everyone on a stable version
    
    * adds useCallback when using useFocusEffect in PodsHomeScreen
    
    * removes unnecessary commented code
    
    * "staging changes"
    
    * merging
    
    * broken: will not update pod for current user before screen is loaded.
    
    * fixes async awaiting to get pod for the current user
    
    * resolving merge conflicts
    
    * updates after merge

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from "expo-status-bar";
 import React, { useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
---
+++ mobile/src/pods/PodsHomeScreen.tsx
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, SafeAreaView, StyleSheet, Button, TextInput } from "react-native";
+import { Formik } from "formik";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { useFocusEffect } from '@react-navigation/native';
+import * as SecureStore from "expo-secure-store";
+
+
+interface Pod {
+  id: number;
+  ownerId: number;
+  name: string;
+}
+
+const PodsHomeScreen = ({ navigation, route }) => {
+  // Holds pod of current user -- undefined if no user exists
+  const [pod, setPod] = useState<Pod>();
+
+  React.useEffect(() => {
+    if (route.params?.pod) {
+      setPod(route.params.pod);
+    }
+  }, [route.params?.pod]);
+
+
+  React.useEffect(() => {
+    async function fetcher() {
+      try {
+        const authToken = await SecureStore.getItemAsync("wigo-auth-token");
+        const res = await fetch(
+          'http://localhost:8000/pods/currUsersPod',
+          {
+            headers: {
+              "Content-Type": "application/json;charset=utf-8",
+              "x-auth-token": authToken!,
+            },
+          }
+        );
+        
+        const json = await res.json();
+        const returnedPod = json.pod;
+        if (returnedPod) {
+          setPod(returnedPod);
+        }
+      } catch (err) {
+        console.log('error loading pod for current user');
+      }
+    };
+
+    fetcher();
+  }, []);
+
+  return (
+      <SafeAreaView style={styles.container}>
+          <View>
+          {pod == null ? (
+              <Button title="Create New Pod" onPress={() => {
+                  console.log('Create button was pressed');
+                  navigation.navigate('CreatePod');
+                  return;
+                }}>
+              </Button>
+          ) : (
+              <Text>Pod Name: {pod.name} </Text>
+          )}
+            </View>
+      </SafeAreaView>
+  )
+};
+
+const styles = StyleSheet.create({
+    container: {
+      display: "flex",
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+  });
+
+export default PodsHomeScreen;
