ec217db3c8797fcdad2b887e849d078fa98c386e
caromurp <57685459+caromurp@users.noreply.github.com>
2020-10-30 11:20:39 -0400

    Schedule now uses auth token to get events for current user

--- api/src/routes/events.ts
+++ api/src/routes/events.ts
@@ -27,12 +27,19 @@ eventRouter.post("/", [auth], async (req: Request, res: Response) => {
   console.log(`Creating event with name '${event.name}' and id '${event.id}'`);
 });
 
-eventRouter.get("/:ownerId", async (req, res) => {
-  console.log("/events/ownerId");
-  const inputOwnerId = req.params.ownerId;
-  console.log("inputOwnerId", inputOwnerId);
-  const eventsForUser = await Event.query().where("ownerId", inputOwnerId);
-  res.json(eventsForUser);
-});
+eventRouter.get(
+  "/currUsersEvents", 
+  [auth], async (req: express.Request, res: express.Response) => {
+      try {
+          const userId = (req as AuthRequest).user.id;
+          const eventsList = await Event.query().where("ownerId", userId);
+          console.log('events', eventsList);
+          res.json({ events: eventsList });
+      } catch (err) {
+          console.error(err);
+          res.status(500).send("Server Error when getting events");
+        }
+  }
+);
 
 export default eventRouter;
