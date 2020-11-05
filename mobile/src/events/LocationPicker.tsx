b0394dfaadf85a8d71d63c0d69a0cd38d147fbce
allysmith12 <allison.smith@seas.upenn.edu>
2020-11-05 11:53:14 -0500

    Added Location Picker to Front End.
    User can choose location via LocationPicker and the value of the location is placed inside Formik.
    
    Note: this does not work bc I removed the api key

---
+++ mobile/src/events/LocationPicker.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { useField, useFormikContext } from "formik";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+
+// // FIGURE OUT HOW TO RESTRICT THIS // encode it 
+
+export interface Place {
+    formatted_address: string,
+    latValue: number,
+    lngValue: number,
+};
+
+interface LocationProps {
+    latFieldName: string; // Formik Field Name
+    lngFieldName: string;
+    formattedAddress: string;
+}
+
+const LocationPicker: React.FC<LocationProps> = (props) => {
+    // Used with setting date value in formik 
+    const { setFieldValue } = useFormikContext();
+    const [latField] = useField(props.latFieldName);
+    const [lngField] = useField(props.lngFieldName);
+    const [formattedAddress] = useField(props.formattedAddress);
+
+    const locationPicked = (data, details) => {
+        console.log("Selected Location")
+        setFieldValue(props.formattedAddress, details?.formatted_address);
+        setFieldValue(props.latFieldName, details?.geometry.location.lat);
+        setFieldValue(props.lngFieldName, details?.geometry.location.lng);
+    }
+
+    return (
+        <GooglePlacesAutocomplete
+            placeholder='Search address'
+            currentLocation={true}
+            fetchDetails={true} // Details in onPress should not be null
+            query={{
+                key: api_key,
+                language: 'en'
+            }}
+            onPress={locationPicked}
+            nearbyPlacesAPI='GooglePlacesSearch'
+            styles={{
+                textInputContainer: {
+                    width: '100%'
+                },
+                description: {
+                    fontWeight: 'bold'
+                },
+                predefinedPlacesDescription: {
+                    color: '#1faadb'
+                }
+            }}
+        />
+    );
+};
+
+export default LocationPicker;
