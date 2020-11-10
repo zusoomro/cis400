47ef50d68fd8912ca1e84c68aa47e473540b438e
zusoomro <zusoomro@seas.upenn.edu>
2020-11-10 18:25:57 -0500

    Adds titles to pod screen names

--- mobile/src/pods/PodsNavigator.tsx
+++ mobile/src/pods/PodsNavigator.tsx
@@ -27,10 +27,26 @@ const PodsNavigator = () => {
 
   return (
     <Stack.Navigator>
-      <Stack.Screen name="PodsHomeScreen" component={PodsHomeScreen} />
-      <Stack.Screen name="CreatePod" component={CreatePod} />
-      <Stack.Screen name="InviteUsers" component={InviteUsers} />
-      <Stack.Screen name="ManageMembers" component={PodMembers} />
+      <Stack.Screen
+        name="PodsHomeScreen"
+        component={PodsHomeScreen}
+        options={{ title: "Your Pods" }}
+      />
+      <Stack.Screen
+        name="CreatePod"
+        component={CreatePod}
+        options={{ title: "Create a Pod" }}
+      />
+      <Stack.Screen
+        name="InviteUsers"
+        component={InviteUsers}
+        options={{ title: "Invite users" }}
+      />
+      <Stack.Screen
+        name="ManageMembers"
+        component={PodMembers}
+        options={{ title: "Manage members" }}
+      />
     </Stack.Navigator>
   );
 };
