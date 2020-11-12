e8e2d081ea9d2f35ac755d267379dfb05b7b83d4
allysmith12 <allison.smith@seas.upenn.edu>
2020-11-12 14:20:26 -0500

    Fix scheduler reload

--- mobile/src/events/Schedule.tsx
+++ mobile/src/events/Schedule.tsx
@@ -55,7 +55,7 @@ const Schedule: React.FC<{}> = ({ navigation }) => {
     }
 
     fetcher();
-  }, []);
+  }, [eventsForUser]);
   let dd = String(today.getDate()).padStart(2, "0");
   let mm = String(today.getMonth() + 1).padStart(2, "0");
   let yyyy = today.getFullYear();
