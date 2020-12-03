95a3a897eca0424f769c49a8ec67d8e029ef166e
caromurp <57685459+caromurp@users.noreply.github.com>
2020-12-03 17:59:07 -0500

    removing delete event code for easier merge. will add in later.

--- api/src/routes/events.ts
+++ api/src/routes/events.ts
@@ -64,15 +64,6 @@ eventRouter.put("/", [auth], async (req: Request, res: Response) => {
     .where("id", eventId);
 });
 
-eventRouter.delete("/", [auth], async (req: Request, res: Response) => {
-  const eventId = req.body.id;
-  console.log("eventid", eventId);
-
-  // const id = (req as AuthRequest).user.id;
-  const event = await Event.query().deleteById(eventId);
-  console.log("event", event);
-  res.json({ event });
-});
 
 eventRouter.get(
   "/apiKey",
