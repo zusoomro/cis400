20c1568e23ae5603e77f9fd1831fbbe8dc96d377
chloeprezelski <57121051+chloeprezelski@users.noreply.github.com>
2020-12-11 10:34:31 -0500

    Send invite post pod creation (#101)
    * adds ability to send invite post pod creation
    
    * cleans up code

--- mobile/src/pods/InviteUsers.tsx
+++ mobile/src/pods/InviteUsers.tsx
@@ -14,6 +14,7 @@ import { List, ListItem, SearchBar } from "react-native-elements";
 import { useSelector } from "react-redux";
 import apiUrl from "../config";
 import sharedStyles from "../sharedStyles";
+import * as SecureStore from "expo-secure-store";
 
 interface User {
   id: number;
@@ -25,6 +26,7 @@ const InviteUsers: React.FC<{}> = ({ navigation, route }) => {
   const [users, setUsers] = useState(u);
   const [invitees, setInvitees] = useState([]);
 
+  const caller = route?.params?.caller;
   const currUserId = useSelector((state) => state.auth.user.id);
 
   React.useEffect(() => {
@@ -38,7 +40,17 @@ const InviteUsers: React.FC<{}> = ({ navigation, route }) => {
         });
 
         const json = await res.json();
-        const result = json.filter((user) => user.id != currUserId);
+        let result = json.filter((user) => user.id != currUserId);
+        const pod = route?.params?.pod;
+        if (pod) {
+          const currMembers: Array<number> = [];
+          pod.members.forEach((member) => {
+            currMembers.push(member.id);
+          });
+          if (currMembers.length) {
+            result = result.filter((user) => !currMembers.includes(user.id));
+          }
+        }
         setUsers(result);
       } catch (err) {
         console.log("error loading users");
@@ -72,8 +84,39 @@ const InviteUsers: React.FC<{}> = ({ navigation, route }) => {
     <UserRowItem title={item.email} user={item} />
   );
 
-  const handleInviteUsers = () => {
-    navigation.navigate("CreatePod", { invitees: invitees });
+  const handleInviteUsers = async () => {
+    if (caller) {
+      if (caller === "PodMembers") {
+        const pod = route?.params?.pod;
+        const data = {
+          inviteeIds: invitees,
+          pod: pod,
+        };
+
+        // make post request to /invites and send the invites
+        try {
+          const res = await fetch(`${apiUrl}/invites`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json;charset=utf-8",
+              "x-auth-token": (await SecureStore.getItemAsync(
+                "wigo-auth-token"
+              ))!,
+            },
+            body: JSON.stringify(data),
+          });
+          const message = await res.json();
+          console.log("message", message);
+          navigation.navigate("PodMembers");
+        } catch (error) {
+          console.log(`error sending invites`, error);
+        }
+      } else if (caller === "CreatePod") {
+        navigation.navigate("CreatePod", { invitees: invitees });
+      } else {
+        console.log("error: i'm confused");
+      }
+    }
   };
 
   return (
