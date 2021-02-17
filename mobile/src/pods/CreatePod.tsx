441e57f7090bdffa9744e15472ca5826f03b5162
chloeprezelski <chloeprezelski@gmail.com>
2021-02-17 15:04:56 -0500

    fixes bug with pod interaction after it is first created

--- mobile/src/pods/CreatePod.tsx
+++ mobile/src/pods/CreatePod.tsx
@@ -4,7 +4,7 @@ import Button from "../shared/Button";
 import { Formik } from "formik";
 import * as SecureStore from "expo-secure-store";
 import apiUrl from "../config";
-import { setPod as reduxSetPod } from "./podSlice";
+import { setPod as reduxSetPod, loadUserPods } from "./podSlice";
 import sharedStyles from "../sharedStyles";
 import { useDispatch, useSelector } from "react-redux";
 import * as Yup from "yup";
@@ -58,6 +58,7 @@ const CreatePod: React.FC<Props> = ({ navigation, route }) => {
                 dispatch(reduxSetPod(pod));
                 setPod(pod);
               }
+              dispatch(loadUserPods());
               navigation.navigate("PodsHomeScreen", { pod: pod });
             }
           }}
