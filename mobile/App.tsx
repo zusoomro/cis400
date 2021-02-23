5efa8b85f39e3a2c054395333ef5619f6cd594ea
zusoomro <zusoomro@seas.upenn.edu>
2021-02-23 07:00:35 -0500

    Filler code for tests

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,3 +1,4 @@
+import analytics from "./src/analytics/analytics";
 import { BebasNeue_400Regular, useFonts } from "@expo-google-fonts/bebas-neue";
 import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
 import { AppLoading } from "expo";
@@ -46,6 +47,91 @@ const ContextApp = () => {
     dispatch(loadToken());
     // In case you need to delete the user token, uncomment this line
     // SecureStore.deleteItemAsync("wigo-auth-token");
+    for (let i = 0; i < 150; i++) {
+      analytics.identify(i.toString());
+      analytics.track("User entered conflicting event");
+    }
+
+    for (let i = 0; i < 60; i++) {
+      analytics.identify(i.toString());
+      analytics.track("Conflict: user scheduled at suggested time");
+    }
+
+    for (let i = 0; i < 60; i++) {
+      analytics.identify(i.toString());
+      analytics.track("Conflict: user edits their event");
+    }
+
+    for (let i = 0; i < 30; i++) {
+      analytics.identify(i.toString());
+      analytics.track("Conflict: user schedules event anyway");
+    }
+
+    for (let i = 0; i < 50; i++) {
+      analytics.identify(i.toString());
+      analytics.track("Event created", {
+        $duration: Math.floor(Math.random() * 60) + 30,
+      });
+    }
+
+    for (let i = 0; i < 50; i++) {
+      analytics.identify(i.toString());
+      analytics.track("Pod created", {
+        $duration: Math.floor(Math.random() * 60) + 45,
+      });
+    }
+
+    for (let i = 0; i < 50; i++) {
+      analytics.identify(i.toString());
+      analytics.track("Viewed schedule page", {
+        $duration: Math.floor(Math.random() * 60) + 60,
+      });
+    }
+
+    for (let i = 0; i < 50; i++) {
+      analytics.identify(i.toString());
+      analytics.track("Viewed pod page", {
+        $duration: Math.floor(Math.random() * 60) + 45,
+      });
+    }
+
+    for (let i = 0; i < 50; i++) {
+      analytics.identify(i.toString());
+      analytics.track("Viewed settings page", {
+        $duration: Math.floor(Math.random() * 60) + 15,
+      });
+    }
+
+    for (let i = 0; i < 47; i++) {
+      analytics.identify(i.toString());
+      analytics.track("User entered conflicting event");
+      analytics.track("Event created");
+      analytics.track("Event modified");
+      analytics.track("Event deleted");
+      analytics.track("Conflict: user scheduled at suggested time");
+      analytics.track("Conflict: user edits thier event");
+      analytics.track("Conflict: user schedules event anyway");
+      analytics.track("Viewing user's events");
+      analytics.track("Viewing pod's events");
+      analytics.track("Log out");
+      analytics.track("Log in");
+      analytics.track("Register");
+      analytics.track("Users invited to pod");
+      analytics.track("Pod created");
+      analytics.track("User rejected invite");
+      analytics.track("User accepted invite");
+      analytics.track("User entered conflicting event");
+      analytics.track("Conflict: user scheduled at suggested time");
+      analytics.track("Conflict: user edits their event");
+      analytics.track("Conflict: user schedules event anyway");
+      analytics.track("Event created");
+      analytics.track("Pod created");
+      analytics.track("Viewed schedule page");
+      analytics.track("Viewed pod page");
+      analytics.track("Viewed settings page");
+    }
+
+    console.log("tracked events!");
   }, []);
 
   useEffect(() => {
--- mobile/src/analytics/analytics.ts
+++ mobile/src/analytics/analytics.ts
@@ -1,5 +1,5 @@
 import ExpoMixpanelAnalytics from "@benawad/expo-mixpanel-analytics";
 
-const analytics = new ExpoMixpanelAnalytics("9746ba72b2f6cef44b62604562650dd6");
+const analytics = new ExpoMixpanelAnalytics("346ec1e6f79e50f6446c8b540ee1391c");
 
 export default analytics;
